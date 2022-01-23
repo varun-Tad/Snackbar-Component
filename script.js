432b54b741115a5d14aa02701c7998d2ed3b7ee9
varun-Tad <varuntad141@gmail.com>
2022-01-23 11:44:26 +0530

    created buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const baselineComp = document.querySelector("#baseline-comp");
+const baselineBtn = document.querySelector("#baseline-btn");
+
+
+
+baselineBtn.addEventListener('click',function(){
+
+    baselineComp.classList.add("hidden");
+})
+
+
+
+
+
